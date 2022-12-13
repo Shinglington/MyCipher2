e13b289044a52b9cdba39b8f3a3c473f3e76e31b
Shinglington <4kshing@gmail.com>
2022-12-13 16:30:49 +0000

    stuff

--- MyCryptanalysis/attack_transposition.py
+++ MyCryptanalysis/attack_transposition.py
@@ -1,21 +1,65 @@
 import random
+from itertools import permutations
 from MyCiphers.rowtrans import RowTrans
 from MyCiphers.coltrans import ColTrans
+from MyCryptanalysis.nbest import nbest
 from MyCryptanalysis.ngram_score import calc_score
 
-def Attack_ColTrans(ciphertext, keylen):
+def Attack_ColTrans(ciphertext, keylen = None):
 	def alg(ciphertext, key):
 		return ColTrans(key).decrypt(ciphertext)
-	Hill_Climb(ciphertext, keylen, alg)
 
-def Attack_RowTrans(ciphertext, keylen):
+	attack_trans(ciphertext, keylen, alg)
+
+
+def Attack_RowTrans(ciphertext, keylen = None):
 	def alg(ciphertext, key):
 		return RowTrans(key).decrypt(ciphertext)
-	Hill_Climb(ciphertext, keylen, alg)
+		
+	attack_trans(ciphertext, keylen, alg)
+
+
+
+def attack_trans(ciphertext, keylen, decryptalg):
+	best_keys = nbest(5)
+	if keylen == None:
+		for i in range(2, 21):
+			print("\n\n\n\n\n\n")
+			print("Trying key length " + str(i))
+			temp_keys = get_best_keys(ciphertext, i, decryptalg)
+			for i in range(len(best_keys)):
+				pair = best_keys[i]
+				print(str(i+1) + ". Key = " + str(pair[0]) + ", score = " + str(pair[1]))
+				print(decryptalg(ciphertext, pair[0]))
+				print("\n\n\n")
+				
+			for j in range(len(temp_keys)):
+				pair = temp_keys[j]
+				best_keys.add(pair[0],pair[1])
+			
+	else:
+		best_keys = get_best_keys(ciphertext, keylen, decryptalg)
+
+	for i in range(len(best_keys)):
+		pair = best_keys[i]
+		print(str(i+1) + ". Key = " + str(pair[0]) + ", score = " + str(pair[1]))
+		print(decryptalg(ciphertext, pair[0]))
+		print("\n\n\n")
+
+def get_best_keys(ciphertext, keylen, decryptalg):
+	best_keys = nbest(5)
+	if keylen < 9:
+		for perm in permutations(list(range(keylen))):
+			key = list(perm)
+			best_keys.add(key, decryptalg(ciphertext, key))
+	else:
+		best_keys = Hill_Climb(ciphertext, keylen, decryptalg)
+	return best_keys
+
 
 def Hill_Climb(ciphertext, keylen, decryptalg):
-	best_key = ""
-	best_score = -99e9
+	best_keys = nbest(5)
+	best_keys.add("",-99e9)
 	key_streak = 0
 	while (key_streak < 10):
 		current_key = GetRandomKey(keylen)
@@ -29,22 +73,16 @@ def Hill_Climb(ciphertext, keylen, decryptalg):
 				current_score = score
 				iterations = 0
 			iterations += 1
-
-		if current_score > best_score:
-			best_score = current_score
-			best_key = current_key
-			print("Score is currently {0}".format(best_score))
-			print(decryptalg(ciphertext, best_key))
-			print("Key = {0}".format(best_key))
+		if current_score > best_keys[0][1]:
+			print("Score is currently {0}".format(current_score))
+			print(decryptalg(ciphertext, current_key))
+			print("Key = {0}".format(current_key))
 			print("\n\n\n")
 			key_streak = 0
 		else:
 			print("Keystreak is now " + str(key_streak+1))
 			key_streak += 1
-
-	print("\n\n\n")
-	print("Key ={0}, score = {1}".format(best_key, best_score))
-	print(decryptalg(ciphertext, best_key[0]))
+		best_keys.add(current_key, current_score)
 
 
 
